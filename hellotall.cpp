9f4fd68ccfc7ebffa05803db2f8de35c766f5d28
blanchescmc <55533903+blanchescmc@users.noreply.github.com>
2023-01-30 10:53:01 +0800

    Create hellotall.cpp

---
+++ hellotall.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World, HAHAHA  " << 1 << " m " << 60 << " cm " << endl;
+    printf("Hello World, your tall is %d m %d cm \n", 1 ,60 );
+    return 0;
+}
